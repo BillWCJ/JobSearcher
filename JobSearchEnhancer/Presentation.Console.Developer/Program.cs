69b0be706b3a0e1344de7a201e3c48cad960a10f
BillWenChaoJiang <jiang.bill@hotmail.com>
2014-10-05 21:15:14 -0400

    Rename Console app

--- JobSearchEnhancer/Presentation.Console.Developer/Program.cs
+++ JobSearchEnhancer/Presentation.Console.Developer/Program.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data.Entity;
-using System.Data.Entity.Infrastructure;
 using System.Globalization;
 using System.Linq;
 using System.Linq.Expressions;
@@ -18,16 +16,13 @@ using Model.Definition;
 using Model.Entities;
 using GlobalVariable;
 using System.Xml;
-using System.Windows.Forms;
-using HtmlAgilityPack;
-using HtmlDocument = HtmlAgilityPack.HtmlDocument;
 using Data.Web.JobMine;
 using Data.EF.ClusterDB;
 using Data.IO.Local;
 
-namespace ConsoleApplication 
+namespace Presentation.Console.Developer
 {
-    public class ConsoleApplication
+    class Program
     {
         static void Main(string[] args)
         {
@@ -35,7 +30,7 @@ namespace ConsoleApplication
             string appsAvail = JobStatus.AppsAvail;
             var client = new CookieEnabledWebClient();
             Seeders(term, appsAvail);
-         
+
 
 
             //DownloadJobsDetailsForAllUser();
@@ -50,32 +45,32 @@ namespace ConsoleApplication
         }
         public static void DownLoadJobs(string term, string jobStatus, string filePath)
         {
-            Console.WriteLine("Enter JobMine UserName");
-            string username = Console.ReadLine();
-            Console.WriteLine("Enter Password");
-            string password = Console.ReadLine();
+            System.Console.WriteLine("Enter JobMine UserName");
+            string username = System.Console.ReadLine();
+            System.Console.WriteLine("Enter Password");
+            string password = System.Console.ReadLine();
             GVar.Account = new UserAccount { Username = username, Password = password };
 
             var client = new CookieEnabledWebClient();
             Queue<string> jobIDs;
 
-            Console.WriteLine("Loggedin : {0}", Login.LoginToJobMine(client).ToString());
+            System.Console.WriteLine("Loggedin : {0}", Login.LoginToJobMine(client).ToString());
             jobIDs = JobInquiry.GetJobIDs(client, term, jobStatus);
             int numjob = jobIDs.Count;
-            Console.WriteLine("Total Number of Jobs Found: {0}", numjob);
+            System.Console.WriteLine("Total Number of Jobs Found: {0}", numjob);
             JobDetail.DownLoadAndWriteJobsToLocal(jobIDs, client, fileLocation: filePath, numJobsPerFile: 100);
         }
         private static void DownloadJobsDetailsForAllUser()
         {
-            Console.WriteLine("Enter The Term (eg 1149)");
-            string term = Console.ReadLine();
-            Console.WriteLine("Enter JobStatus (one of the following option: {0},{1},{2},{3})", JobStatus.Approved,
+            System.Console.WriteLine("Enter The Term (eg 1149)");
+            string term = System.Console.ReadLine();
+            System.Console.WriteLine("Enter JobStatus (one of the following option: {0},{1},{2},{3})", JobStatus.Approved,
                 JobStatus.AppsAvail, JobStatus.Cancelled, JobStatus.Posted);
-            Console.WriteLine("They are Approved, AppsAvail, Cancelled, and Posted respectively");
-            string jobStatus = Console.ReadLine();
-            Console.WriteLine(@"Please Enter the File Path (eg. C:\Users\BillWenChao\Desktop\  - have to end with '\')");
+            System.Console.WriteLine("They are Approved, AppsAvail, Cancelled, and Posted respectively");
+            string jobStatus = System.Console.ReadLine();
+            System.Console.WriteLine(@"Please Enter the File Path (eg. C:\Users\BillWenChao\Desktop\  - have to end with '\')");
             string filePath = @"C:\Users\BillWenChao\Desktop\";
-            filePath = Console.ReadLine();
+            filePath = System.Console.ReadLine();
             DownLoadJobs(term, jobStatus, filePath);
 
         }
@@ -83,7 +78,7 @@ namespace ConsoleApplication
         private static void DownloadWriteOpenHtmlData(CookieEnabledWebClient client, string downloadUrl, string htmlFileName)
         {
             File.WriteAllText(GVar.FilePath + htmlFileName, client.DownloadString(downloadUrl));
-            Console.WriteLine("{0} - Opening: {1}", Login.LoginToJobMine(client), htmlFileName);
+            System.Console.WriteLine("{0} - Opening: {1}", Login.LoginToJobMine(client), htmlFileName);
             Process.Start(GVar.FilePath + htmlFileName);
         }
     }
--- JobSearchEnhancer/Presentation.Console.Developer/Properties/AssemblyInfo.cs
+++ JobSearchEnhancer/Presentation.Console.Developer/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Console")]
+[assembly: AssemblyTitle("Presentation.Console.Developer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Console")]
+[assembly: AssemblyProduct("Presentation.Console.Developer")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("423dea3c-877c-4acc-956a-456052f8a449")]
+[assembly: Guid("4721f663-b22c-4193-a595-ecd7dc0c7fbe")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
