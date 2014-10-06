63bdb0675e6b92ef31ead186315a4821bb7d72ae
BillWenChaoJiang <jiang.bill@hotmail.com>
2014-10-05 20:30:49 -0400

    Updated business

---
+++ JobSearchEnhancer/Business.JobMine/DownloadManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data.Web.JobMine;
+using GlobalVariable;
+using Model.Entities;
+
+namespace Business.JobMine
+{
+    public class DownloadManager
+    {
+        public static void DownLoadJobs(string username, string password, string term, string jobStatus, string filePath)
+        {
+            GVar.Account = new UserAccount { Username = username, Password = password };
+
+            var client = new CookieEnabledWebClient();
+            Queue<string> jobIDs;
+
+            Console.WriteLine((string) "Loggedin : {0}", (object) Login.LoginToJobMine(client).ToString());
+            jobIDs = JobInquiry.GetJobIDs(client, term, jobStatus);
+            int numjob = jobIDs.Count;
+            Console.WriteLine("Total Number of Jobs Found: {0}", numjob);
+            JobDetail.DownLoadAndWriteJobsToLocal(jobIDs, client, fileLocation: filePath, numJobsPerFile: 100);
+        }
+    }
+}
--- JobSearchEnhancer/TextDownload/JobMine.cs
+++ JobSearchEnhancer/TextDownload/JobMine.cs
@@ -8,18 +8,5 @@ namespace TextDownload
 {
     public class JobMine
     {
-        public static void DownLoadJobs(string username, string password, string term, string jobStatus, string filePath)
-        {
-            GVar.Account = new UserAccount { Username = username, Password = password };
-
-            var client = new CookieEnabledWebClient();
-            Queue<string> jobIDs;
-
-            Console.WriteLine("Loggedin : {0}", Login.LoginToJobMine(client).ToString());
-            jobIDs = JobInquiry.GetJobIDs(client, term, jobStatus);
-            int numjob = jobIDs.Count;
-            Console.WriteLine("Total Number of Jobs Found: {0}", numjob);
-            JobDetail.DownLoadAndWriteJobsToLocal(jobIDs, client, fileLocation: filePath, numJobsPerFile: 100);
-        }
     }
 }
