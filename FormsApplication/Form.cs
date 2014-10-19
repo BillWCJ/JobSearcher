d502c35ec07b1e56f60dec841841e88f80687f82
BillWenChaoJiang <jiang.bill@hotmail.com>
2014-10-19 01:22:40 -0400

    Move Files around mostly because visual studio can't publish

---
+++ FormsApplication/Form.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Business.Account;
+using Model.Entities;
+using Data.Web.JobMine;
+using Model.Definition;
+
+namespace FormsApplication
+{
+    public partial class JobDetailBrowser : Form
+    {
+        Job[] Jobs { get; set; }
+        int _currentJobIndex = 0;
+
+        private void DisplayCurrentJobDetail()
+        {
+
+            SectionTabControl.SelectedIndex = 0;
+            JobDetailRichTextBox.Text = Jobs[_currentJobIndex].ToString("f");
+        }
+
+        public JobDetailBrowser()
+        {
+            InitializeComponent();
+            Jobs = new TextParser(new AccountManager().Account).ReadInJobFromLocal();
+            DisplayCurrentJobDetail();
+            CommonWebBrowser.Url = new Uri(JobMineDef.LogInUrl);
+        }
+
+        private void PreviousJobButton_Click(object sender, EventArgs e)
+        {
+            if (--_currentJobIndex < 0)
+                _currentJobIndex = 0;
+            DisplayCurrentJobDetail();
+        }
+
+        private void NextJobButton_Click(object sender, EventArgs e)
+        {
+            if (++_currentJobIndex >= Jobs.Length)
+                _currentJobIndex = Jobs.Length - 1;
+            DisplayCurrentJobDetail();
+        }
+
+        private void JobDetailRichTextBox_LinkClicked(object sender, LinkClickedEventArgs e)
+        {
+            string url = e.LinkText;
+            if (url.IndexOf(@"http://") != 0 && url.IndexOf(@"https://") !=0)
+                url = @"http://" + url;
+            CommonWebBrowser.Url = new Uri(url);
+            SectionTabControl.SelectedIndex = 2;
+        }
+    }
+}
