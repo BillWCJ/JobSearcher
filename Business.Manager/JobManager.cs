467613faf47b4a6e9977cf9e544e2bdaa9f7727b
Paul Soladoye <psoladoye@gmail.com>
2014-12-16 10:48:36 -0500

    Initial JobManager Implementation

---
+++ Business.Manager/JobManager.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data.EF.ClusterDB;
+using Model.Entities;
+
+namespace Business.Manager
+{
+    public static class JobManager
+    {
+        public static TermType GetTermDuration(Job job=null)
+        {
+            var value = (TermType) 0;
+            using (var db = new DatabaseContext())
+            {
+                
+                foreach (var j in db.Jobs)
+                {
+                    if (string.IsNullOrWhiteSpace(j.Comment))
+                    {
+                        if (string.IsNullOrWhiteSpace(j.JobDescription))
+                            throw new ArgumentException("Empty description field");
+
+                        value = CheckString(j.JobDescription);
+                        Console.ForegroundColor = ConsoleColor.DarkRed;
+                        Console.WriteLine("Job title: {0}\nJob description: {1}\nTerm Duration: {2}\n JobId: {3} ",
+                            j.JobTitle,j.JobDescription,value, j.Id);
+                        Console.ResetColor();
+                        Console.WriteLine("-----------------------------------------------------");
+                    }
+                    else
+                    {
+                        value = CheckString(j.Comment);
+                        if (value == TermType.Not_Specified)
+                            value = CheckString(j.JobDescription);
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine("Job title: {0}\nJob comment: {1}\nTerm Duration: {2}\n JobId: {3}",
+                            j.JobTitle, j.Comment, value, j.Id);
+                        Console.ResetColor();
+                        Console.WriteLine("-----------------------------------------------------");
+                    }
+
+                }
+            }
+            return value;
+        }
+
+        private static TermType CheckString(string comment)
+        {
+            if ((comment.ToLower().Contains("4 month") &&
+                comment.ToLower().Contains("8 month")) ||
+                (comment.ToLower().Contains("4 months") &&
+                comment.ToLower().Contains("8 months")) ||
+                (comment.ToLower().Contains("4 or 8 months")))
+            {
+                return TermType.Both;
+            }
+
+            if (comment.ToLower().Contains("4 months") ||
+                comment.ToLower().Contains("4 month"))
+            {
+                return TermType.Four;
+            }
+
+            if (comment.ToLower().Contains("8 months") ||
+                comment.ToLower().Contains("8 month"))
+            {
+                return TermType.Eight;
+            }
+
+            return TermType.Not_Specified;
+        }
+    }
+}
--- DevelopmentConsole/Program.cs
+++ DevelopmentConsole/Program.cs
@@ -1,27 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Linq.Expressions;
-using System.Text;
-using System.Threading.Tasks;
-using System.IO;
-using System.Collections.Specialized;
-using System.Net;
-using System.Windows;
 using System.Diagnostics;
+using System.IO;
 using Business.Account;
 using Business.DataBaseSeeder;
-using Business.JobMine;
-using Data.Web.GoogleApis;
+using Business.Manager;
+using Data.IO.Local;
+using Data.Web.JobMine;
 using Model.Definition;
 using Model.Entities;
-using System.Xml;
-using Data.Web.JobMine;
-using Data.EF.ClusterDB;
-using Data.IO.Local;
 
-namespace Presentation.Console.Developer
+namespace DevelopmentConsole
 {
     class Program
     {
@@ -30,10 +19,14 @@ namespace Presentation.Console.Developer
             const string term = "1151";
             const string appsAvail = JobStatus.AppsAvail;
             var client = new CookieEnabledWebClient();
-            Seeders(term, appsAvail, new AccountManager().Account);
+            //Seeders(term, appsAvail, new AccountManager().Account);
 
             //DownloadJobsDetailsForAllUser();
             //DownLoadJobs("w52jiang", "Ss332640747:)","1149", GVar.JobStatus.AppsAvail, @"C:\Users\BillWenChao\Desktop\");
+
+            Console.WriteLine("DBContext");
+            JobManager.GetTermDuration();
+            Console.ReadLine();
         }
 
         private static void Seeders(string term, string appsAvail, UserAccount account)
@@ -44,31 +37,31 @@ namespace Presentation.Console.Developer
         }
         public static void DownLoadJobs(string term, string jobStatus, string filePath, UserAccount account)
         {
-            System.Console.WriteLine("Enter JobMine UserName");
-            string username = System.Console.ReadLine();
-            System.Console.WriteLine("Enter Password");
-            string password = System.Console.ReadLine();
+            Console.WriteLine("Enter JobMine UserName");
+            string username = Console.ReadLine();
+            Console.WriteLine("Enter Password");
+            string password = Console.ReadLine();
             account = new UserAccount { Username = username, Password = password };
 
             var client = new CookieEnabledWebClient();
 
-            System.Console.WriteLine("Loggedin : {0}", new Login(account).LoginToJobMine(client).ToString());
+            Console.WriteLine("Loggedin : {0}", new Login(account).LoginToJobMine(client).ToString());
             Queue<string> jobIDs = JobInquiry.GetJobIDs(client, term, jobStatus);
             int numjob = jobIDs.Count;
-            System.Console.WriteLine("Total Number of Jobs Found: {0}", numjob);
+            Console.WriteLine("Total Number of Jobs Found: {0}", numjob);
             JobDetail.DownLoadAndWriteJobsToLocal(jobIDs, client, account, fileLocation: filePath, numJobsPerFile: 100);
         }
         private static void DownloadJobsDetailsForAllUser()
         {
-            System.Console.WriteLine("Enter The Term (eg 1149)");
-            string term = System.Console.ReadLine();
-            System.Console.WriteLine("Enter JobStatus (one of the following option: {0},{1},{2},{3})", JobStatus.Approved,
+            Console.WriteLine("Enter The Term (eg 1149)");
+            string term = Console.ReadLine();
+            Console.WriteLine("Enter JobStatus (one of the following option: {0},{1},{2},{3})", JobStatus.Approved,
                 JobStatus.AppsAvail, JobStatus.Cancelled, JobStatus.Posted);
-            System.Console.WriteLine("They are Approved, AppsAvail, Cancelled, and Posted respectively");
-            string jobStatus = System.Console.ReadLine();
-            System.Console.WriteLine(@"Please Enter the File Path (eg. C:\Users\BillWenChao\Desktop\  - have to end with '\')");
+            Console.WriteLine("They are Approved, AppsAvail, Cancelled, and Posted respectively");
+            string jobStatus = Console.ReadLine();
+            Console.WriteLine(@"Please Enter the File Path (eg. C:\Users\BillWenChao\Desktop\  - have to end with '\')");
             string filePath = @"C:\Users\BillWenChao\Desktop\";
-            filePath = System.Console.ReadLine();
+            filePath = Console.ReadLine();
             DownLoadJobs(term, jobStatus, filePath, new AccountManager().Account);
 
         }
@@ -77,7 +70,7 @@ namespace Presentation.Console.Developer
         {
             File.WriteAllText(account.FilePath + htmlFileName, client.DownloadString(downloadUrl));
             Login login = new Login(account);
-            System.Console.WriteLine("{0} - Opening: {1}", login.LoginToJobMine(client), htmlFileName);
+            Console.WriteLine("{0} - Opening: {1}", login.LoginToJobMine(client), htmlFileName);
             Process.Start(account.FilePath + htmlFileName);
         }
     }
