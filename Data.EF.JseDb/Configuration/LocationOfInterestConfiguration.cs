f53e4a2ef3b2983b02c5a267e5fe4eb3f965d3ba
BillWenChaoJiang <jiang.bill@hotmail.com>
2014-12-21 03:47:46 -0500

    location of interest

---
+++ Data.EF.JseDb/Configuration/LocationOfInterestConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity.ModelConfiguration;
+using Model.Entities;
+
+namespace Data.EF.JseDb.Configuration
+{
+    public class LocationOfInterestConfiguration : EntityTypeConfiguration<JobLocation>
+    {
+        public LocationOfInterestConfiguration()
+        {
+            Property(c => c.Longitude).HasPrecision(18, 10);
+            Property(c => c.Latitude).HasPrecision(18, 10);
+        }
+    }
+}
---
+++ Model.Entities/LocationOfInterest.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Model.Entities
+{
+    public class LocationOfInterest
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public string FullAddress { get; set; }
+        public string Region { get; set; }
+        public decimal Longitude { get; set; }
+        public decimal Latitude { get; set; }
+    }
+}
