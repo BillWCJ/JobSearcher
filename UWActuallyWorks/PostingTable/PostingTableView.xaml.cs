0e172e37293578136a1a7c917a493684a908049a
BillWenChaoJiang <jiang.bill@hotmail.com>
2015-07-21 23:47:18 -0400

    Add UWActuallyWork WPF -Added Prism -Added Table to display data along with its viewmodels

---
+++ UWActuallyWorks/PostingTable/PostingTableView.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UWActuallyWorks.PostingTable
+{
+    /// <summary>
+    /// Interaction logic for PostingTable.xaml
+    /// </summary>
+    public partial class PostingTableView : UserControl
+    {
+        public PostingTableViewModel ViewModel;
+        public PostingTableView()
+        {
+            InitializeComponent();
+            this.ViewModel = new PostingTableViewModel();
+            this.JobPostingTable.ItemsSource = ViewModel.JobPostings;
+        }
+    }
+}
---
+++ UWActuallyWorks/PostingTable/PostingTableViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+using Business.DataBaseSeeder;
+using Business.Manager;
+using Data.EF.JseDb;
+using Data.IO.Local;
+using Model.Definition;
+using Model.Entities.JobMine;
+
+namespace UWActuallyWorks.PostingTable
+{
+    public class PostingTableViewModel
+    {
+        public ObservableCollection<JobPostingViewModel> JobPostings { get; private set; }
+
+        protected JobReviewManager JobReviewManager;
+        
+        public PostingTableViewModel()
+        {
+            JobReviewManager = new JobReviewManager();
+            var jobs = JobSearcher.FindJobs();
+            var jobPostingViewModels = jobs.Select(job => new JobPostingViewModel(job));
+            JobPostings = new ObservableCollection<JobPostingViewModel>(jobPostingViewModels);
+        }
+    }
+}
