6d15186f36dcfd30f8ec4268c2d5cbc2cad93826
BillWenChaoJiang <jiang.bill@hotmail.com>
2015-09-28 14:48:36 -0400

    Update config 2

--- Presentation.WPF/Events/CommonEvents.cs
+++ Presentation.WPF/Events/CommonEvents.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Microsoft.Practices.Prism.Events;
 using Microsoft.Practices.Prism.PubSubEvents;
 using Model.Entities;
 using Model.Entities.JobMine;
